577aaf1d688410cc7c82253d6f4521d4d6bde60a
jiaxin <pujx@codoon.com>
2018-03-04 22:24:56 +0800

    Initial Commit

---
+++ BaseRepository/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BaseRepository
+//
+//  Created by jiaxin on 2018/3/4.
+//  Copyright © 2018年 jiaxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
