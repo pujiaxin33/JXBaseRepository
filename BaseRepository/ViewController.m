1ba8ab4f2be1d3983bf0ef44fa552212a7beb212
pujiaxin33 <317437084@qq.com>
2018-03-05 15:59:56 +0800

    删除测试代码

--- BaseRepository/ViewController.m
+++ BaseRepository/ViewController.m
@@ -17,9 +17,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-
-    NSString *first = @"";
-    NSString *sencod = @"";
 }
 
 
