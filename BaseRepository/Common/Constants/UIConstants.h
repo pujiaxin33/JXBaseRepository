c9138cace68dfe5686ea20e17a9379ed600eb845
imp <yanpeng@codoon.com>
2018-03-06 17:38:44 +0800

    添加屏幕旋转和scrollView显示空白处理

--- BaseRepository/Common/Constants/UIConstants.h
+++ BaseRepository/Common/Constants/UIConstants.h
@@ -18,3 +18,16 @@
 
 #define COLOR_WITH_RGB(r,g,b,a)      [UIColor colorWithRed:(r/255.0) green:(g/255.0) blue:(b/255.0) alpha:a]
 #define COLOR_WITH_HEX(rgbValue,a)   COLOR_WITH_RGB((float)((rgbValue & 0xFF0000) >> 16),(float)((rgbValue & 0xFF00) >> 8),(float)(rgbValue & 0xFF),a)
+
+//针对scrollView顶部空白问题
+#define  adjustsScrollViewInsets_NO(scrollView,vc)\
+do { \
+_Pragma("clang diagnostic push") \
+_Pragma("clang diagnostic ignored \"-Warc-performSelector-leaks\"") \
+if ([UIScrollView instancesRespondToSelector:NSSelectorFromString(@"setContentInsetAdjustmentBehavior:")]) {\
+[scrollView   performSelector:NSSelectorFromString(@"setContentInsetAdjustmentBehavior:") withObject:@(2)];\
+} else {\
+vc.automaticallyAdjustsScrollViewInsets = NO;\
+}\
+_Pragma("clang diagnostic pop") \
+} while (0)
