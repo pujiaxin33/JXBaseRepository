39092ae4a361802c9b86891c0ac52f09f08301c1
pujiaxin33 <317437084@qq.com>
2018-03-06 11:36:19 +0800

    文件目录细化

--- BaseRepository/Managers/Date/CDCalendarManager.h
+++ BaseRepository/Managers/Date/CDCalendarManager.h
@@ -12,7 +12,7 @@
 
 //firstWeekday为默认值1
 @property (nonatomic, strong) NSCalendar *defaultCalendar;
-+ (instancetype)defaultManager;
++ (instancetype)sharedInstance;
 /**
  *  注意：获取到的NSCalendar实例是全局共享的，不允许做属性修改。如果需要自定义，请使用calendar实例方法获取新的对象，并进行单独配置。
  */
--- BaseRepository/Managers/Date/CDDateFormatterManager.h
+++ BaseRepository/Managers/Date/CDDateFormatterManager.h
@@ -8,9 +8,12 @@
 
 #import <Foundation/Foundation.h>
 
+#define DateFormat_YYYY_MM_DD               @"yyyy-MM-dd"
+#define DateFormat_YYYY_MM_DD_HH:MM:SS      @"yyyy-MM-dd HH:mm:ss"
+
 @interface CDDateFormatterManager : NSObject
 
-+ (instancetype)defaultManager;
++ (instancetype)sharedInstance;
 /**
  *  注意：获取到的NSDateFormatter实例是全局共享的，不允许做属性修改。如果需要多次设置dateFormat，请多次调用该方法，维护多个实例。如果需要设置dateFormat以外的属性，请使用dateFormatter实例方法获取新的对象，并进行单独配置。
  */
